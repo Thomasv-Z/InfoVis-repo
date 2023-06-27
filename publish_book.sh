c1040df96ed522b7d675d74d4775369739314dc4
Thomas van Z <thomas@zuilenvan.nl>
2023-06-27 16:51:20 +0200

    Commited changes to Github-Pages

--- publish_book.sh
+++ publish_book.sh
@@ -1,6 +1,5 @@
 conda activate
 conda activate jupyterbook
-jupyter nbconvert --to html --TagRemovePreprocessor.remove_cell_tags='{"hide_code"}' docs/data_story.ipynb
 jupyter-book build --all .
 git add .
 git status
